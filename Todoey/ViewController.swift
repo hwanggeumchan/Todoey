3b4ffd495a9dbaa88508237a25cc12eb85176f1f
HGC <hwanggeumchan@yahoo.com>
2019-04-08 18:09:07 -0700

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Andy Hwang on 4/8/19.
+//  Copyright © 2019 GCH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
